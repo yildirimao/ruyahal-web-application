019e306218280fd7c3a74ed451a23addeed211cb
Ahmet Yildirim <60722461+yildirimao@users.noreply.github.com>
2022-05-15 00:08:00 -0400

    commit

---
+++ ruyahal_backend/ruyahal/src/main/java/com/rh/ruyahal/RuyahalApplication.java
@@ -0,0 +1,13 @@
+package com.rh.ruyahal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuyahalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuyahalApplication.class, args);
+	}
+
+}
---
+++ ruyahal_backend/ruyahal/src/test/java/com/rh/ruyahal/RuyahalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rh.ruyahal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuyahalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
