60074aae6b2e246ce0af18f619c867329b2c11fe
Ahmet Yildirim <60722461+yildirimao@users.noreply.github.com>
2022-05-16 12:24:15 -0400

    working on authentication

--- ruyahal_backend/src/main/java/com/rh/RuyahalApplication.java
+++ ruyahal_backend/src/main/java/com/rh/RuyahalApplication.java
@@ -1,4 +1,4 @@
-package com.rh.ruyahal;
+package com.rh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- ruyahal_backend/src/test/java/com/rh/RuyahalApplicationTests.java
+++ ruyahal_backend/src/test/java/com/rh/RuyahalApplicationTests.java
@@ -1,4 +1,4 @@
-package com.rh.ruyahal;
+package com.rh;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
